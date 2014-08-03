e65c16fb5a5b62c6840e86127dcff9a977fee724
Sean Walberg <sean@ertw.com>
2014-08-03 17:54:47 -0500

    Fix pid returning wrong value, hook up lcd

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 
 #include "display.h"
 #include "spi.h"
+#include "motor.h"
 
 typedef struct {
     double error;
@@ -20,24 +21,29 @@ typedef struct {
     double temperature;
     double accumulated_error;
     double last_error;
+    double last_value;
+    unsigned long int last_time;
 } pidInfoStruct;
 
 char mode = 0;
 int foo = 100;
 char digit;
 pidInfoStruct pidData;
+unsigned long int current_time;
 
 void setup(void) {
 
     pidData.accumulated_error = 0;
     pidData.setpoint = 225;
     pidData.error = 0;
+    current_time = 0;
     
     ADCON1 = 0x06; // All pins digital
     ADCON0 = 0; // A/D off
     
     TRISA = 0b00000000;
     TRISB = 0b00000000; // B0 is CS for the temperature sensor
+                        // B1:4 are for the stepper motor
     TRISD = 0x00; // PORTD is the 7 segment LED
     TRISE = 0x00; // PORTE controls the status of the LED
     TRISC = 0b00010000; // C4 is SPI data in
@@ -49,7 +55,7 @@ void setup(void) {
     TMR0 = 0;
     T0CON = 0b11010100; // enable Timer0 as 8 bit, 1:32 prescale
 
-    T1CON = 0b00110001; // 1:8 prescale, Fosc/4
+    T1CON = 0b00100001; // 1:4 prescale, Fosc/4
 
     INTCONbits.GIE = 1;     // Enable global interrupts
     INTCONbits.PEIE = 1;    // Enable peripheral interrupts
@@ -72,160 +78,88 @@ void setup(void) {
     SSPEN=1;            // SPI enable!
 }
 
-double percentage_error(double measured, double setpoint) {
-    return(measured/setpoint);
+double calculate_error(double measured, double setpoint) {
+    return(setpoint - measured);
 }
-void /* interrupt */ tc_int(void) {
+
+void interrupt tc_int(void) {
     static volatile char tmr1count = 0;
     
     
     if (T0IE && T0IF) {  // Timer 0 resets quickly
         T0IF = 0;
-//        displayDigit(digit, pickDigit(digit, mode == 0 ? (int)pidData.temperature : foo));
+        displayDigit(digit, pickDigit(digit, mode == 0 ? (int)pidData.temperature : foo));
         digit++;
         digit %= 3;
         return;
     }
 
-    if (TMR1IE && TMR1IF) {
-        if (tmr1count++ == 10) { // every 2 seconds
-            tmr1count = 0;
-           // PORTAbits.RA0 = ! PORTAbits.RA0;
-
-            // update accumulated_error
-            if (pidData.error != 0) {
-                pidData.accumulated_error += pidData.error;
-                if(pidData.accumulated_error > 500) {
-                    pidData.accumulated_error = 500;
-                }
-            }
-        }
+    if (TMR1IE && TMR1IF) { // every .1 seconds
+ //       PORTDbits.RD2 = ! PORTDbits.RD2;
+        current_time++;
         TMR1IF=0;
     }
 }
 
-
-double run_pid(double error, pidInfoStruct *pid) {
+double run_pid(pidInfoStruct *pid) {
 
     double p, i, d;
+    double output;
 
-    p = 2.0 * error;
-    i = 3.0 * pid->accumulated_error;
-    d = 4.0 * (error - pid->last_error);
-
-    pid->last_error = error;
-
-    return p + i + d;
-}
-void stepper_delay(void) {
-    int i;
-    for (i=0; i < 1; i++) {
-        __delay_ms(5);
-    }
-}
-
-#define ON 1
-#define OFF 0
-
-void blue(int status) {
-    PORTB &= 0b11111101;
-    PORTB |= (status << 1);
-}
-
-void pink(int status) {
-    PORTB &= 0b11111011;
-    PORTB |= (status << 2);
-}
-
-void yellow(int status) {
-    PORTB &= 0b11110111;
-    PORTB |= (status << 3);
-}
-
-void orange(int status) {
-    PORTB &= 0b11101111;
-    PORTB |= (status << 4);
-}
-
-void forward(int steps) {
-    int i;
-    for (i=0; i < steps; i++) {
-        blue(ON);
-        pink(OFF);
-        yellow(OFF);
-        orange(OFF);
-
-        stepper_delay();
-
-        blue(OFF);
-        pink(ON);
-        yellow(OFF);
-        orange(OFF);
-
-        stepper_delay();
-
-        blue(OFF);
-        pink(OFF);
-        yellow(ON);
-        orange(OFF);
-
-        stepper_delay();
-
-        blue(OFF);
-        pink(OFF);
-        yellow(OFF);
-        orange(ON);
-
-        stepper_delay();
+#define PID_FREQUENCY 10 // every second
+    if (current_time - pid->last_time < PID_FREQUENCY) {
+        return(pid->last_value);
     }
 
+    pid->temperature = readTemperature();
+
+    pid->error = pid->setpoint - pid->temperature;
+    pid->accumulated_error += pid->error;
+    if (pid->accumulated_error > 1000) {
+        pid->accumulated_error = 1000;
+    }
+    if (pid->accumulated_error < -1000) {
+        pid->accumulated_error = -1000;
+    }
+    
+    p = 2 * pid->error;
+    i = 2 * pid->accumulated_error;
+    d = pid->error - pid->last_error;
+    
+    pid->last_error = pid->error;
+    pid->last_time = current_time;
+    
+    output = p + i + d;
+    pid->last_value = output;
+
+   return(output);
 }
-
-void backward(int steps) {
-    int i;
-    for (i=0; i < steps; i++) {
-        blue(OFF);
-        pink(OFF);
-        yellow(OFF);
-        orange(ON);
-
-        stepper_delay();
-
-        blue(OFF);
-        pink(OFF);
-        yellow(ON);
-        orange(OFF);
-
-        stepper_delay();
-
-        blue(OFF);
-        pink(ON);
-        yellow(OFF);
-        orange(OFF);
-
-        stepper_delay();
-
-        blue(ON);
-        pink(OFF);
-        yellow(OFF);
-        orange(OFF);
-
-        stepper_delay();
-
+int move_to(int from, int to) {
+    if (to < 0) {
+        to = 0;
+    }
+    if (to > 1000) {
+        to = 1000;
     }
 
+    if (from > to) {
+        forward(from - to);
+    } else {
+        backward(to - from);
+    }
+    return(to);
 }
-
-
 int main(void) {
 
-    int d;
+    int d, current_position;
+    double setting;
+
+    current_position = 0;
     setup();
-   
+    PORTD=0xff;
     while(1) {
-       pidData.temperature = readTemperature();
-       pidData.error = percentage_error(pidData.temperature, pidData.setpoint);
-        
+       setting = run_pid(&pidData);
+       current_position = move_to(current_position, (int)pidData.last_value);
         __delay_ms( 10 );
     }
    
